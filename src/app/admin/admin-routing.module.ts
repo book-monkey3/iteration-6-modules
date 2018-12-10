a8244f1599b8f9eebf23b41d431d9813261a2886
The Buildbot <buildbot@angular2buch.de>
2018-12-10 22:58:27 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/2a5bb7db573f2432d3b32d9b330a22dc3e83a141
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/186

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CreateBookComponent } from './create-book/create-book.component';
-import { EditBookComponent } from './edit-book/edit-book.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'create',
-    pathMatch: 'full'
-  },
-  {
-    path: 'create',
-    component: CreateBookComponent
-  },
-  {
-    path: 'edit/:isbn',
-    component: EditBookComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { CanNavigateToAdminGuard } from './can-navigate-to-admin.guard';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { CreateBookComponent } from './admin/create-book/create-book.component';
+import { EditBookComponent } from './admin/edit-book/edit-book.component';
 
 const routes: Routes = [
   {
@@ -16,18 +19,29 @@ const routes: Routes = [
   },
   {
     path: 'books',
-    loadChildren: 'src/app/books/books.module#BooksModule'
+    component: BookListComponent
+  },
+  {
+    path: 'books/:isbn',
+    component: BookDetailsComponent
   },
   {
     path: 'admin',
-    loadChildren: 'src/app/admin/admin.module#AdminModule',
-    canActivate: [CanNavigateToAdminGuard]
+    redirectTo: 'admin/create',
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin/create',
+    component: CreateBookComponent
+  },
+  {
+    path: 'admin/edit/:isbn',
+    component: EditBookComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [CanNavigateToAdminGuard]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
